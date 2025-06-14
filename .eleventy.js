d5ac11d6dae7c80a3d8eb343f209439611c78a33
Matt Arnold <matt.mattarn@gmail.com>
2025-06-14 14:22:56 -0400

    Tag cloud

--- .eleventy.js
+++ .eleventy.js
@@ -31,6 +31,17 @@ module.exports = function(eleventyConfig) {
     return collectionApi.getFilteredByGlob('src/blog/posts/*.md').sort((a, b) => b.date - a.date);
   });
 
+  // Add a collection for all unique tags
+  eleventyConfig.addCollection('allTags', function(collectionApi) {
+    const tagSet = new Set();
+    collectionApi.getFilteredByGlob('src/blog/posts/*.md').forEach(item => {
+      if (Array.isArray(item.data.tags)) {
+        item.data.tags.forEach(tag => tagSet.add(tag));
+      }
+    });
+    return Array.from(tagSet).sort((a, b) => a.localeCompare(b, undefined, {sensitivity: 'base'}));
+  });
+
   return {
     dir: { input: 'src', output: '_site' }
   };
